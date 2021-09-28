1254f83cf23a1e5c30c9b9f08af6efded74374e5
williamjeon7 <williamjeon7@kookmin.ac.kr>
2021-09-28 14:24:51 +0900

    GPIO LED 제어 과제

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,22 @@
+#define PIN_LED 7
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(PIN_LED, 0);
+    delay(1000);
+    for(int i=0; i<=4; i+=1)
+    {
+      digitalWrite(PIN_LED, 1);
+      delay(100);
+      digitalWrite(PIN_LED, 0);
+      delay(100);
+    }
+    digitalWrite(PIN_LED, 1);
+    while(1){
+
+    }
+    
+}
